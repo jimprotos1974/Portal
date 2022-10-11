5d77096b01b50b9f2498aa3b4c78709c765d1397
jimprotos1974 <49305456+jimprotos1974@users.noreply.github.com>
2022-10-11 14:23:57 +0300

    2 files modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { ProductComponent } from './product/product.component';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
 
+import { UserComponent } from './user/user.component';
+
 const routes: Routes = [
   { path: 'product', component: ProductComponent },
   { path: 'product/:id', component: ProductDetailComponent },
+  
+  { path: 'user', component: UserComponent },
 ];
 
 @NgModule({
--- src/code/util/api/dummy.ts
+++ src/code/util/api/dummy.ts
@@ -9,25 +9,52 @@ import { Api } from './api';
 export class Dummy extends Api{
   override url: string = 'https://dummyjson.com';
   override entities: any = {
+    user: {
+      browse: {
+        method: 'GET',
+        permission: 'Permissions.User.View',
+        url: '/users',
+      },
+      locate: {
+        method: 'GET',
+        permission: 'Permissions.User.View',
+        url: '/users/{id}',
+      },
+      create: {
+        method: 'POST',
+        permission: 'Permissions.User.Create',
+        url: '/users',
+      },
+      update: {
+        method: 'PUT',
+        permission: 'Permissions.User.Edit',
+        url: '/users/{id}',
+      },
+      remove: {
+        method: 'DELETE',
+        permission: 'Permissions.User.Delete',
+        url: '/users/{id}',
+      },
+    },
     product: {
       browse: {
         method: 'GET',
-        permission: 'Permissions.Company.View',
+        permission: 'Permissions.Product.View',
         url: '/products',
       },
       locate: {
         method: 'GET',
-        permission: 'Permissions.Company.View',
+        permission: 'Permissions.Product.View',
         url: '/products/{id}',
       },
       create: {
         method: 'POST',
-        permission: 'Permissions.Company.Create',
+        permission: 'Permissions.Product.Create',
         url: '/products',
       },
       update: {
         method: 'PUT',
-        permission: 'Permissions.Company.Edit',
+        permission: 'Permissions.Product.Edit',
         url: '/products/{id}',
       },
       remove: {
