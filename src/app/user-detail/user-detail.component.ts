91115fcb2c58d4c105402bea2c8f230607a9403b
jimprotos1974 <49305456+jimprotos1974@users.noreply.github.com>
2022-10-11 16:35:10 +0300

    2 files modified

--- src/app/user-detail/user-detail.component.ts
+++ src/app/user-detail/user-detail.component.ts
@@ -8,31 +8,22 @@ import { User as UserEntity } from '../../code/util/entity/user';
 @Component({
   selector: 'app-user-detail',
   templateUrl: './user-detail.component.html',
-  styleUrls: ['./user-detail.component.css']
+  styleUrls: ['./user-detail.component.css'],
 })
-
 export class UserDetailComponent implements OnInit {
-
   id?: number | undefined;
   user: UserInterface | undefined;
 
-  constructor(private route: ActivatedRoute, private api: DummyApi) {
-
-  }
+  constructor(private route: ActivatedRoute, private api: DummyApi) {}
 
   ngOnInit(): void {
     this.id = parseInt(this.route.snapshot.paramMap.get('id')!, 10);
     let entity = new UserEntity(this.api);
 
-    entity.locate(this.id).subscribe(response => {
-      console.log(response)
+    entity.locate(this.id).subscribe((response) => {
+      console.log(response);
 
       this.user = new UserModel(response).convert();
     });
   }
-
-  ngOnChanges(changes: any): void{
-    console.log("xxx");
-  };
-
 }
